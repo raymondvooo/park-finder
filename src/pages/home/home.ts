fb35a8240dfa2a2ad01531c42154d81cd8223160
unknown <raymondvooo@gmail.com>
2018-10-17 02:20:28 -0700

    map complete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,70 +2,91 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { MapProvider } from '../../providers/map/map';
- 
+
 declare var google;
- 
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
- 
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
- 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation,  public mapProvider: MapProvider) {
- 
-  }
- 
-  ionViewDidLoad(){
-    this.loadMap();
-  }
- 
-  loadMap(){
- 
-    this.geolocation.getCurrentPosition().then((position) => {
- 
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    selector: 'page-home',
+    templateUrl: 'home.html'
+  })
+
+  export class HomePage {
+    @ViewChild('map') mapElement: ElementRef;
+
+      map: any = {};
+      infowindow: any = {};
+    
+   
+    constructor(public navCtrl: NavController, public geolocation: Geolocation,  public mapProvider: MapProvider) {
+        
+    }
+    ionViewDidLoad(){
+        this.initMap();
       }
+    initMap() {
+        setTimeout(() => {   
+            
+            this.geolocation.getCurrentPosition().then((position) => {
  
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
-    }, (err) => {
-      console.log(err);
-    });
- 
-  }
+                let myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+           
+                let mapOptions = {
+                  center: myLocation,
+                  zoom: 15,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+           
+                this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  addInfoWindow(marker, content){
- 
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
-   
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
-   
-  }
+                new google.maps.Marker({
+                    position: myLocation,
+                    icon: '  http://maps.google.com/mapfiles/ms/icons/arrow.png',
+                    map: this.map
+                  });
 
-  addMarker(){
- 
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
-    });
-   
-    let content = "<h4>Information!</h4>";         
-   
-    this.addInfoWindow(marker, content);
-   
-  }
- 
+                
+         
+
+
+            this.infowindow = new google.maps.InfoWindow();
+            var service = new google.maps.places.PlacesService(this.map);
+            service.nearbySearch({
+                location: myLocation,
+                radius: 5000,
+                type: ['store']
+            }, (result, status) => this.callback(result, status, this));
+           
+              }, (err) => {
+                console.log(err);
+              });
+
+
+        }, 1000);
+    }
+
+    createMarker(place, that) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: that.map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          that.infowindow.setContent(place.name);
+          that.infowindow.open(that.map, marker);
+          console.log(place)
+        });
+    }
+    
+  
+
+    callback(results, status, that) { 
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+            that.createMarker(results[i], that);
+            }
+        }
+    }
+    
+
 }
